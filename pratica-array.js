4ee1d9aad9dac3248102005012e3b35161e9f790
FrancinaldoAlves <francifilho07@gmail.com>
2025-10-10 07:46:55 -0300

    ativida pratica em javascript

--- pratica-array.js
+++ pratica-array.js
@@ -1,12 +1,12 @@
 const nomes = ["fracinaldo", "josé", "pedro", "lucas", "tiago"];
 nomes.forEach((nome) => console.log("nome:", nome));
 
-console.log("---------------------------------------------")
+console.log("---------------------------------------------");
 
 const numeros = [1,2,3,4,5];
 const arrayDobro = numeros.map((numero) => console.log("numero dobrado:", numero * 2));
 
-console.log("---------------------------------------------")
+console.log("---------------------------------------------");
 
 const arrayImpares = numeros.filter((numero) => (numero % 2 === 0));
 console.log("numeros ímpares:", arrayImpares);
